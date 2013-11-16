3e927a1306321f5623cf804e68f28bc03295e086
alejandrodob <alejandrodob@gmail.com>
2013-11-16 17:48:51 +0000

    Created tickets controller

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,78 @@
+class TicketsController < ApplicationController
+  before_action :set_ticket, only: [:show, :edit, :update, :destroy]
+  before_action :set_trip, only: [:index, :new, :create]
+
+  # GET /tickets
+  # GET /tickets.json
+  def index
+  end
+
+  # GET /tickets/1
+  # GET /tickets/1.json
+  def show
+  end
+
+  # GET /tickets/new
+  def new
+    @ticket = @trip.tickets.build
+  end
+
+  # GET /tickets/1/edit
+  def edit
+  end
+
+  # POST /tickets
+  # POST /tickets.json
+  def create
+    @ticket = Ticket.new(ticket_params)
+
+    respond_to do |format|
+      if @ticket.save
+        format.html { redirect_to @trip, notice: 'Ticket was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @trip }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @ticket.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /tickets/1
+  # PATCH/PUT /tickets/1.json
+  def update
+    respond_to do |format|
+      if @ticket.update(ticket_params)
+        format.html { redirect_to @trip, notice: 'Ticket was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @ticket.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /tickets/1
+  # DELETE /tickets/1.json
+  def destroy
+    @ticket.destroy
+    respond_to do |format|
+      format.html { redirect_to tickets_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_ticket
+      @ticket = Ticket.find(params[:ticket_id] || params[:id])
+    end
+
+    def set_trip
+      @trip = Trip.find(params[:trip_id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def ticket_params
+      params.require(:ticket).permit(:origin, :destination, :means_of_transport, :seat, :extra_info)
+    end
+end
