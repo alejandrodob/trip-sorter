9e05949bdf93867adf1523b8a69e224f60097b55
alejandrodob <alejandrodob@gmail.com>
2013-11-17 15:56:52 +0000

    Implemented sorting algorithm

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,7 +3,25 @@ class Trip < ActiveRecord::Base
 
 
   def sort_tickets
-    sorted = []
-    unsorted = tickets
+    if tickets.empty?
+      []
+    else
+      unsorted = tickets.to_a
+      sorted = [] << unsorted.shift
+      anti_infinite_loop_counter = 0
+      while !unsorted.empty? && anti_infinite_loop_counter < 100000
+        ticket = unsorted.shift
+        if ticket.origin == sorted[-1].destination
+          sorted.push ticket
+        elsif ticket.destination == sorted[0].origin
+          sorted.unshift ticket
+        else
+          unsorted.unshift ticket
+          unsorted.shuffle!
+        end
+        anti_infinite_loop_counter += 1
+      end
+      sorted
+    end
   end
 end
